30381df56a76b90c7f018ace37e3a0935893fb82
pabloduke <pabloduke@email.com>
2023-11-02 22:19:46 -0500

    adding ability to cycle through questions, keep track of total correct and display if the user was right or wrong

--- flashCards.js
+++ flashCards.js
@@ -1,52 +1,55 @@
-function fetchJSON() {
-    return fetch("questions.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Failed to fetch data from ${"questions.json"}`);
-            }
-            return response.json();
-        });
-}
+function askQuestion() {
+    let questionList = getQuestionsList()
+    let index =parseInt(document.getElementById("question_number").value)
+    let questions = questionList["questions"]
+        let items = questions[index]
 
-function askQuestion(data) {
-    for (key in data) {
-        let questions = data[key]
-        for (question in questions) {
-            let items = questions[question]
+        document.getElementById("Question").innerText = items["Question"]
 
-            // console.log(items["Question"])
-            document.getElementById("Question").innerText = items["Question"]
-            document.getElementById("a_answer_label").innerText = items["a_answer"]
-            document.getElementById("a_answer_radio").value = "a"
+        document.getElementById("a_answer_label").innerText = items["a_answer"]
+        document.getElementById("a_answer_radio").value = "a"
 
-            document.getElementById("b_answer_label").innerText = items["b_answer"]
-            document.getElementById("b_answer_radio").value = "b"
+        document.getElementById("b_answer_label").innerText = items["b_answer"]
+        document.getElementById("b_answer_radio").value = "b"
 
-            document.getElementById("c_answer_label").innerText = items["c_answer"]
-            document.getElementById("c_answer_radio").value = "c"
+        document.getElementById("c_answer_label").innerText = items["c_answer"]
+        document.getElementById("c_answer_radio").value = "c"
 
-            document.getElementById("d_answer_label").innerText = items["d_answer"]
-            document.getElementById("d_answer_radio").value = "d"
-        }
-    }
+        document.getElementById("d_answer_label").innerText = items["d_answer"]
+        document.getElementById("d_answer_radio").value = "d"
 }
 
 function submit_answer() {
+    let questionMap = getQuestionsList()
+    let questionNumber = parseInt(document.getElementById("question_number").value)
+
+    let questionList = questionMap["questions"]
+    let question = questionList[questionNumber]
+    let correct_answer = question["correct_answer"]
+    let user_answer = "f"
+    let total_questions = questionList.length
+
     let formElement = this.document.getElementById("answer_question")
     for (let answer in formElement) {
-        if(formElement[answer].checked) {
-            document.getElementById("user_answer").innerText = formElement[answer].value
+        if (formElement[answer].checked === true) {
+            user_answer = formElement[answer].value
+            console.log(user_answer)
+            formElement[answer].checked = false
+            let divAnswer = document.getElementById("result")
+            let total_correct = parseInt(document.getElementById("total_correct").value) + 1
+            document.getElementById("total_correct").value = total_correct
+            if (user_answer === correct_answer) {
+                divAnswer.innerText = "CORRECT"
+                document.getElementById("total_correct_display").innerText = "(" + total_correct + " / " + total_questions + ")"
+            } else {
+                divAnswer.innerText = "WRONG"
+            }
+            break;
         }
     }
 
-}
 
-// Usage
-const url = '"questions.json"'; // Replace with your JSON file URL
-fetchJSON(url)
-    .then(data => {
-        askQuestion(data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+
+    document.getElementById("question_number").value = `${questionNumber + 1}`
+    askQuestion()
+}
---
+++ questions.js
@@ -0,0 +1,119 @@
+function getQuestionsList() {
+    return {
+        "questions": [
+            {
+                "Question": "Which of the above did NOT write a Gospel?",
+                "a_answer": "Mark",
+                "b_answer": "Luke",
+                "c_answer": "Matthew",
+                "d_answer": "George",
+                "correct_answer": "d"
+            },
+            {
+                "Question": "Of the listed players, who is the best basketball player?",
+                "a_answer": "Magic Johnson",
+                "b_answer": "Michael Jordan",
+                "c_answer": "Ronald Regan",
+                "d_answer": "Bo Jackson",
+                "correct_answer": "b"
+            },
+            {
+                "Question": "Who was the 1st baseman for the 87' Minnesota Twins?",
+                "a_answer": "Kent Hrbek",
+                "b_answer": "Kirby Puckett",
+                "c_answer": "Harmon Killebrew",
+                "d_answer": "Lou Gehri g",
+                "correct_answer": "a"
+            },
+            {
+                "Question": "Which one of these Vana family pets loved to eat Twinkies?",
+                "a_answer": "Pablo",
+                "b_answer": "Duke",
+                "c_answer": "Rocky II",
+                "d_answer": "Butch",
+                "correct_answer": "b"
+            },
+            {
+                "Question": "Which philosopher is considered a Doctor of the Church?",
+                "a_answer": "Bertrand Russel",
+                "b_answer": "Yogi Berra",
+                "c_answer": "Plato",
+                "d_answer": "Thomas Aquinas",
+                "correct_answer": "d"
+            },
+            {
+                "Question": "Which American President placed people of Japanese descent into internment camps?",
+                "a_answer": "Donald Trump",
+                "b_answer": "Franklin Delano Roosevelt (the commie)",
+                "c_answer": "Abraham Lincoln (the dictator)",
+                "d_answer": "Joe Biden (the moron)",
+                "correct_answer": "b"
+            },
+            {
+                "Question": "What is it called in baseball when the count is 3 balls and 2 strikes?",
+                "a_answer": "Bases Loaded",
+                "b_answer": "Hat Trick",
+                "c_answer": "Triple Double",
+                "d_answer": "Full Count",
+                "correct_answer": "d"
+            },
+            {
+                "Question": "How long is a Major penalty in hockey?",
+                "a_answer": "2 years",
+                "b_answer": "5 minutes",
+                "c_answer": "4 feet",
+                "d_answer": "30 seconds",
+                "correct_answer": "b"
+            },
+            {
+                "Question": "What famous Catholic wrote Lord of the Rings?",
+                "a_answer": "C.S. Lewis",
+                "b_answer": "G. K. Chesterton",
+                "c_answer": "Sun Tzsu",
+                "d_answer": "J.R.R. Tolkien",
+                "correct_answer": "d"
+            },
+            {
+                "Question": "What Roman Emperor was murdered by 40 senators?",
+                "a_answer": "Nero",
+                "b_answer": "Caligula",
+                "c_answer": "Palpatine",
+                "d_answer": "Julius Caesar",
+                "correct_answer": "d"
+            },
+            {
+                "Question": "What Spartan King led 300 men into battle against an invading Persian army?",
+                "a_answer": "Leonidas",
+                "b_answer": "Arhtur",
+                "c_answer": "Theodan",
+                "d_answer": "Charles",
+                "correct_answer": "a"
+            },
+            {
+                "Question": "Which of these Generals is undefeated?",
+                "a_answer": "Scipio Africanus",
+                "b_answer": "Maximus Decimus Maridius",
+                "c_answer": "Alexandar the Great",
+                "d_answer": "Napolean",
+                "correct_answer": "c"
+            },
+            {
+                "Question": "Which economic system is evil?",
+                "a_answer": "Barter System",
+                "b_answer": "Communism",
+                "c_answer": "Free Market (capitalism)",
+                "d_answer": "Post-Scarcity",
+                "correct_answer": "b"
+            },
+            {
+                "Question": "What is the name of the Super Bowl trophy?",
+                "a_answer": "Stanley Cup",
+                "b_answer": "The Commissioner's Trophy",
+                "c_answer": "Killer Cow Award",
+                "d_answer": "Lombardi Trophy",
+                "correct_answer": "d"
+            }
+        ]
+    }
+}
+
